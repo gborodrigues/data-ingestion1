5ceae69f29aaf109b430cc28f8c4b6965062535d
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2024-08-04 17:11:20 -0300

    feat: add pandas

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import os
-import csv
+import pandas as pd
 
 def connect_to_db():
     db_config = {
@@ -20,43 +20,20 @@ def connect_to_db():
         print(f"Error: {err}")
 
 
-def read_csv_files_in_directory(directory_path, encoding='utf-8'):
+def read_csv_files_in_directory(directory_path):
     files = os.listdir(directory_path)
-    
     csv_files = [file for file in files if file.endswith('.csv')]
-    
     for csv_file in csv_files:
         file_path = os.path.join(directory_path, csv_file)
         try:
             print(f"Reading file: {file_path}")
-            qtd = 0
-
-            with open(file_path, mode='r', newline='', encoding=encoding) as file:
-                csv_reader = csv.reader(file)
-                header = next(csv_reader, None)
-                
-                if header is None:
-                    print(f"Skipping empty file: {file_path}")
-                    continue
-
-                has_rows = False
-                
-                for row_number, row in enumerate(csv_reader, start=2):
-                    has_rows = True
-                    try:
-                        # Processing each row
-                        qtd += 1
-                    except Exception as row_err:
-                        print(f"Error processing row {row_number} in file {file_path}: {row_err}")
-                        print(f"Row content: {row}")
-                
-                if not has_rows:
-                    print(f"Skipping empty file (only header present): {file_path}")
-                    continue
-
+            df = pd.read_csv(file_path, sep='|', encoding='latin-1')
+            qtd = len(df)
             print(f"File: {file_path} has {qtd} lines")
-        except UnicodeDecodeError as e:
-            print(f"Skipping file {file_path} due to encoding error: {e}")
+        except UnicodeDecodeError:
+            if df.empty:
+                print(f"Skipping empty file: {file_path}")
+                continue
         except Exception as file_err:
             print(f"Error reading file {file_path}: {file_err}")
 
@@ -64,7 +41,7 @@ def read_csv_files_in_directory(directory_path, encoding='utf-8'):
 if __name__ == "__main__":
     try:
         connect_to_db()
-        directories_paths = ['Empregados', 'Bancos', 'ReclamaçΣes']
+        directories_paths = ['Bancos', 'Empregados', 'ReclamaçΣes']
         for diretory in directories_paths:
             read_csv_files_in_directory(diretory)
         print('fim')
