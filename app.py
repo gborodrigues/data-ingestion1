95ea4676ec216493ad2ab4280ba0edb17075dd4b
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2024-08-05 21:47:13 -0300

    feat: merge

--- app.py
+++ app.py
@@ -27,7 +27,12 @@ def read_csv_files_in_directory(directory_path):
     for csv_file in csv_files:
         file_path = os.path.join(directory_path, csv_file)
         try:
-            df = pd.read_csv(file_path, sep='|', encoding='latin-1')
+            if directory_path == "Bancos":
+                df = pd.read_csv(file_path, sep='\t', encoding='latin-1')
+            elif directory_path == "Empregados":
+                df = pd.read_csv(file_path, sep='|', encoding='latin-1')
+            else:
+                df = pd.read_csv(file_path, sep=';', encoding='latin-1')
             dataframes.append(df)
         except pd.errors.EmptyDataError:
             print(f"Skipping empty file: {file_path}")
@@ -37,8 +42,27 @@ def read_csv_files_in_directory(directory_path):
     return merged_df
 
 
-def clean_string(df, field, pattern):
-    df[f"{field}_cleaned"] = df[field].str.replace(pattern, '', regex=True)
+def clean_string(df, field):
+    pattern = (
+        r' - PRUDENCIAL|'
+        r' S\.A[./]?|'
+        r' S/A[/]?|'
+        r'GRUPO|'
+        r' SCFI|'
+        r' CC |'
+        r' C\.C |'
+        r' CCTVM[/]?|'
+        r' LTDA[/]?|'
+        r' DTVM[/]?|'
+        r' BM[/]?|'
+        r' CH[/]?|'
+        r'COOPERATIVA DE CRÉDITO, POUPANÇA E INVESTIMENTO D[E?O?A/]?|'
+        r' [(]conglomerado[)]?|'
+        r'GRUPO[ /]|'
+        r' -[ /]?'
+    )
+
+    df["campo_limpo"] = df[field].str.replace(pattern, '', regex=True)
     return df
 
 
@@ -46,11 +70,14 @@ if __name__ == "__main__":
     try:
         connect_to_db()
         directories_paths = ['Bancos', 'Empregados', 'ReclamaçΣes']
-        dataframes = []
+        dataframes = {}
         for diretory in directories_paths:
             dataframe = read_csv_files_in_directory(diretory)
-            dataframes.append(dataframe)
-        print(len(dataframes))
+            dataframes[diretory] = dataframe
+        dataframes['Bancos'] = clean_string(dataframes['Bancos'], 'Nome')
+        dataframes['Empregados'] = clean_string(dataframes['ReclamaçΣes'], 'Instituição financeira')
+        merged_df = pd.merge(dataframes['Bancos'], dataframes['ReclamaçΣes'], on=["campo_limpo"])
+        print(merged_df)
     except KeyboardInterrupt:
         print("Interrupted by user")
     except Exception as e:
